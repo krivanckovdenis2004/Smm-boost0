ca2f96715bcf60b0d635ee422e3bc19789e40f42
krivanckovdenis2004 <krivanckovdenis2004@icloud.com>
2026-05-08 20:50:54 +0300

    Add Firebase integration and enhance setupCalculator
    Integrate Firebase for order management and update calculator functionality.

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-function setupCalculator(inputId, totalId, pricePer1000, serviceName) {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCPhcoKEW9O1soc_bbBHWmitjaoZwHrfL8",
+
+  authDomain: "smm-boost-905d5.firebaseapp.com",
+
+  projectId: "smm-boost-905d5",
+
+  storageBucket: "smm-boost-905d5.firebasestorage.app",
+
+  messagingSenderId: "554912523069",
+
+  appId: "1:554912523069:web:26d405b696b9d45e5edb54",
+
+  measurementId: "G-E6SRLXZW5V"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+let currentService = '';
+let currentAmount = 0;
+let currentPrice = 0;
+
+function setupCalculator(
+  inputId,
+  totalId,
+  pricePer1000,
+  serviceName
+) {
 
   const input = document.getElementById(inputId);
 
@@ -27,6 +66,12 @@ function setupCalculator(inputId, totalId, pricePer1000, serviceName) {
 
     const price =
       (amount / 1000) * pricePer1000;
+
+    currentService = serviceName;
+
+    currentAmount = amount;
+
+    currentPrice = price;
 
     document.getElementById('serviceName')
       .innerText =
@@ -88,5 +133,57 @@ document
 
     document.getElementById('orderModal')
       .style.display = 'none';
+
+});
+
+document
+  .getElementById('payButton')
+  .addEventListener('click', async () => {
+
+    const link =
+      document.getElementById('instagramLink')
+      .value;
+
+    if (!link) {
+
+      alert('Введите ссылку');
+
+      return;
+
+    }
+
+    try {
+
+      await addDoc(collection(db, 'orders'), {
+
+        service: currentService,
+
+        amount: currentAmount,
+
+        price: currentPrice,
+
+        link: link,
+
+        status: '🟡 В обработке',
+
+        createdAt: new Date()
+
+      });
+
+      alert('Заказ успешно создан 🔥');
+
+      document.getElementById('orderModal')
+        .style.display = 'none';
+
+      document.getElementById('instagramLink')
+        .value = '';
+
+    } catch (e) {
+
+      console.log(e);
+
+      alert('Ошибка создания заказа');
+
+    }
 
 });
