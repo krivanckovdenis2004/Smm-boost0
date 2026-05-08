1e2eb392514c09d56effb1332bf7638b7d13ca58
krivanckovdenis2004 <krivanckovdenis2004@icloud.com>
2026-05-08 22:01:53 +0300

    Add files via upload

---
+++ track.js
@@ -0,0 +1,101 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+
+  getFirestore,
+
+  doc,
+
+  getDoc
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCPhcoKEW9O1soc_bbBHWmitjaoZwHrfL8",
+
+  authDomain: "smm-boost-905d5.firebaseapp.com",
+
+  projectId: "smm-boost-905d5",
+
+  storageBucket: "smm-boost-905d5.firebasestorage.app",
+
+  messagingSenderId: "554912523069",
+
+  appId: "1:554912523069:web:26d405b696b9d45e5edb54",
+
+  measurementId: "G-E6SRLXZW5V"
+
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+const params =
+new URLSearchParams(window.location.search);
+
+const orderId =
+params.get('id');
+
+const orderInfo =
+document.getElementById('orderInfo');
+
+async function loadOrder() {
+
+  if (!orderId) {
+
+    orderInfo.innerHTML =
+      'ID заказа не найден';
+
+    return;
+
+  }
+
+  const orderRef =
+    doc(db, "orders", orderId);
+
+  const orderSnap =
+    await getDoc(orderRef);
+
+  if (orderSnap.exists()) {
+
+    const order =
+      orderSnap.data();
+
+    orderInfo.innerHTML = `
+
+      <p>
+      <b>Услуга:</b>
+      ${order.service}
+      </p>
+
+      <p>
+      <b>Количество:</b>
+      ${order.amount}
+      </p>
+
+      <p>
+      <b>Сумма:</b>
+      ${order.price}₽
+      </p>
+
+      <p>
+      <b>Статус:</b>
+      ${order.status}
+      </p>
+
+    `;
+
+  } else {
+
+    orderInfo.innerHTML =
+      'Заказ не найден';
+
+  }
+
+}
+
+loadOrder();
