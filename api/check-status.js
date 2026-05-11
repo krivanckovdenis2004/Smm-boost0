c95b121a70483856739eb1b654185303afc15d0a
krivanckovdenis2004 <krivanckovdenis2004@icloud.com>
2026-05-11 14:16:21 +0300

    Update check-status.js

--- api/check-status.js
+++ api/check-status.js
@@ -1,31 +1,8 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCPhcoKEW9O1soc_bbBHWmitjaoZwHrfL8",
-  authDomain: "smm-boost-905d5.firebaseapp.com",
-  projectId: "smm-boost-905d5",
-  storageBucket: "smm-boost-905d5.firebasestorage.app",
-  messagingSenderId: "554912523069",
-  appId: "1:554912523069:web:26d405b696b9d45e5edb54"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 export default async function handler(req, res) {
-
-  const snapshot = await getDocs(
-    collection(db, "orders")
-  );
 
   return res.status(200).json({
     success: true,
-    total: snapshot.size
+    message: "API works"
   });
 
 }
