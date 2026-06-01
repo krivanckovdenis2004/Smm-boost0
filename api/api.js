a4f49c6c3f48b59bc8fcb48998ba2ca6f16ffca8
krivanckovdenis2004 <krivanckovdenis2004@icloud.com>
2026-06-01 14:11:25 +0300

    Delete api/api.js

--- api/api.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(403).json({
-    error: 'Direct JAP order endpoint is disabled for security. Orders are created only after confirmed payment webhook.'
-  });
-}
